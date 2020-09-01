e777b7e04b1666e14904a6394b0aacb49071e942
davide-amabile <davideamabile81@gmail.com>
2020-09-01 17:17:10 +0200

    evento show/hide lista

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var sparire = false;
+
+
 $(".fas").click( function () {
+  if (sparire == false){
   $(".header-right ul").show("fast");
+  sparire = true;
+ } else {
+  $(".header-right ul").hide("fast");
+  sparire = false;
+  }
  }
 );
